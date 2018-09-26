62911273b27b231d50e23914c576deaef3ae114f
maxence_coupet <maxence.coupet@gmail.com>
2018-09-26 19:05:29 +0200

    new templating engine

--- test_report.py
+++ test_report.py
@@ -28,16 +28,6 @@ def create_report(id, autocall):
     if platform.system() == 'Linux':
 
         latex_jinja_env = jinja2.Environment(
-        	block_start_string = '\BLOCK{',
-        	block_end_string = '}',
-        	variable_start_string = '\VAR{',
-        	variable_end_string = '}',
-        	comment_start_string = '\#{',
-        	comment_end_string = '}',
-        	line_statement_prefix = '%%',
-        	line_comment_prefix = '%#',
-        	trim_blocks = True,
-        	autoescape = False,
         	loader = jinja2.FileSystemLoader(os.path.abspath('./reports/'))
         )
         template = latex_jinja_env.get_template('report-template.html')
